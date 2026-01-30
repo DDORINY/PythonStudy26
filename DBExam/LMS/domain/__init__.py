37c9e73785b6244b66a7b4cc1a84030e09115f04
320-11\jihun2240 <jihun22400669@gmail.com>
2026-01-30 17:57:53 +0900

    import pymysql (MySQL_DB를 Python에 불러와 회원관리 CRUD 실습)

---
+++ DBExam/LMS/domain/__init__.py
@@ -0,0 +1,6 @@
+from LMS.domain.Member import Member
+from LMS.domain.Score import Score
+from LMS.domain.Item import Item
+from LMS.domain.Board import Board
+
+__all__ = ["Member", "Score", "Item", "Board"]
